254a90bad793aa85d1b8a465c00a9ffcf212ab4b
CarlijnR <37950298+CarlijnR@users.noreply.github.com>
2018-04-24 13:38:34 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/tap2order/Tap2orderApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.tap2order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tap2orderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tap2orderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/tap2order/Tap2orderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.tap2order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tap2orderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
