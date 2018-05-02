9303331a67289c400dbd3656e70738ab9cee3f27
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-05-02 19:45:48 +0200

    fix

--- src/main/java/com/capgemini/tap2order/Tap2orderApplication.java
+++ src/main/java/com/capgemini/tap2order/Tap2orderApplication.java
@@ -2,7 +2,7 @@ package com.capgemini.tap2order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.capgemini.tap2order.controller.OrderController;
+//import com.capgemini.tap2order.controller.OrderController;
 
 
 @SpringBootApplication
