9db29ad8d5dbf6e2103a22dd42bd3fb5afd1de4f
CarlijnR <37950298+CarlijnR@users.noreply.github.com>
2018-05-02 16:00:25 +0200

    Updated some front end stuff

--- src/main/resources/static/food.js
+++ src/main/resources/static/food.js
@@ -2,9 +2,11 @@ $.get("api/restaurant/menu/list/food", function(data){
     for(var i=0; i < data.length;i++){
         $('#foodlist').append(
             '<tr>' +
-            '<td>' + data[i].name + '</td>' +
+            '<td class="h5">' + data[i].name + '</td>' +
+            '<td>' + "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim." +
+            '</td>' +
+            '<td><button class="btn btn-grey btn-sm mr-1">-</button>0<button class="btn btn-grey btn-sm ml-1">+</button></td>' +
             '<td>' + data[i].price + '</td>' +
-            '<td><button class="btn bg-warning">Order item</button></td>' +
             '</tr>'
         );
     }
