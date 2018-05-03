557cef6e0aa1b84d92fc9c64057020d77d31a5b2
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-05-03 14:19:34 +0200

    Drink.js food.js updated

--- src/main/resources/static/drinks.js
+++ src/main/resources/static/drinks.js
@@ -1,60 +1,133 @@
-function getDrinkItems() {
-    $.get("/api/drink/", function (data) {
-        if (data.length <= 0) {return;}
-        $('#drinksText').empty();
-        for (var i = 0; i < data.length; i++) {
-            $('#drinksText').append(
-                '<tr>' +
-                '<td>' + data[i].drinkName + '</td>' +
-                '<td>' + data[i].drinkPrice + '</td>' +
-                '<tr>'
-            );
+//function getDrinkItems() {
+//    $.get("/api/drink/", function (data) {
+//        if (data.length <= 0) {return;}
+//        $('#drinksText').empty();
+//        for (var i = 0; i < data.length; i++) {
+//            $('#drinksText').append(
+//                '<tr>' +
+//                '<td>' + data[i].drinkName + '</td>' +
+//                '<td>' + data[i].drinkPrice + '</td>' +
+//                '<tr>'
+//            );
+//        }
+//    });
+//}
+//
+//// function getDrinkItems() {
+////     $.get("/api/drink/", function (data) {
+////         if (data.length <= 0) {return;}
+////         $('#drinksText').empty();
+////         for (var i = 0; i < data.length; i++) {
+////             $('#drinksText').DataTable(
+////                 '<tr>' +
+////                 '<td>' + data[i].drinkName + '</td>' +
+////                 '<td>' + data[i].drinkPrice + '</td>' +
+////                 '<tr>'
+////             );
+////         }
+////     });
+//// }
+//
+//
+//
+//function saveDrinkItems(b) {
+//    b.preventDefault();
+//
+//    var nameDrink = $("#nameDrink").val();
+//    var priceDrink = $("#priceDrink").val();
+//
+//    $.post("/api/drink/add",{
+//        drinkName: nameDrink,
+//        drinkPrice: priceDrink
+//      },
+//    getDrinkItems
+//    )
+//};
+//
+////function removeIngredient(){
+////    $.get("/api/ingredient/remove/{id}", function(data){
+////        if(data.length<=0){return;}
+////        var html = "";
+////        for(var i=0;i<data.length;i++){
+////        html +="<p>" + data[i].ingredientId;
+////        data[i].ingredientId = id;
+////    },)};
+//
+//
+//$("#saveDrink").submit(saveDrinkItems);
+
+function postData() {
+    console.log("posting data...");
+
+    // Get values from html.
+    var name = $("#nameDrink").val();
+    var price = $("#priceDrink").val();
+
+
+    // Create JS object with data.
+    var newDrink = {
+        drinkName : name,
+        drinkPrice : price
+    };
+    console.log(newDrink);
+
+    // Convert JS object to JSON.
+    var validJsonDrink = JSON.stringify(newDrink);
+    console.log(validJsonDrink);
+
+    // Post JSON to endpoint.
+    $.ajax({
+        url:"/api/drink/add",
+        type:"post",
+        data: validJsonDrink,
+        contentType: "application/json",
+        success: function(result) {
+            // On successful post, reload data to get the added one as well.
+            console.log("success post data!");
+            getData();
         }
     });
 }
 
-// function getDrinkItems() {
-//     $.get("/api/drink/", function (data) {
-//         if (data.length <= 0) {return;}
-//         $('#drinksText').empty();
-//         for (var i = 0; i < data.length; i++) {
-//             $('#drinksText').DataTable(
-//                 '<tr>' +
-//                 '<td>' + data[i].drinkName + '</td>' +
-//                 '<td>' + data[i].drinkPrice + '</td>' +
-//                 '<tr>'
-//             );
-//         }
-//     });
-// }
+function getData() {
+    console.log("getting data...");
 
+    // Get the data from endpoint.
+    $.ajax({
+        url:"/api/drink/",
+        type:"get",
+        success: function(drinks) {
+            // On successful get, reload the datatable with new data.
+            console.log("This is the data: " + drinks);
+            $('#drinks').DataTable().clear();
+            $('#drinks').DataTable().rows.add(ingredients);
+            $('#drinks').DataTable().columns.adjust().draw();
+        }
+    });
+}
 
+function setup() {
+    // Modal submit.
+    $("#saveDrink").on('submit', function(e) {
+        console.log("Submitted new menu item form");
 
-function saveDrinkItems(b) {
-    b.preventDefault();
+        // Post the data from the modal.
+        postData();
 
-    var nameDrink = $("#nameDrink").val();
-    var priceDrink = $("#priceDrink").val();
+        // Reset modal to hide and no values.
+        $('#newDrinkModal').modal('hide');
+        $("#nameDrink").val("");
+        $("#priceDrink").val("");
+    });
 
-    $.post("/api/drink/add",{
-        drinkName: nameDrink,
-        drinkPrice: priceDrink
-      },
-    getDrinkItems
-    )
-};
+    // Load DataTable with data format.
+    $('#drinks').DataTable({
+        columns: [
+            { "data": "drinkName" },
+            { "data": "drinkPrice" }
+        ]
+    });
 
-//function removeIngredient(){
-//    $.get("/api/ingredient/remove/{id}", function(data){
-//        if(data.length<=0){return;}
-//        var html = "";
-//        for(var i=0;i<data.length;i++){
-//        html +="<p>" + data[i].ingredientId;
-//        data[i].ingredientId = id;
-//    },)};
-
-
-$("#saveDrink").submit(saveDrinkItems);
-
-
-//$("drinkbutton").click(getDrinkItems);
+    // Load first data.
+    getData();
+}
--- src/main/resources/static/food.js
+++ src/main/resources/static/food.js
@@ -1,40 +1,117 @@
-function getMenuItems() {
-    $.get("/api/menuItem/", function (data) {
-        if (data.length <= 0) {return;}
-        $('#menuItem').empty();
-        for (var i = 0; i < data.length; i++) {
-            $('#menuItem').append(
-            '<tr>' +
-                '<td>' + data[i].menuItemName + '</td>' +
-                '<td>' + data[i].menuItemPrice + '</td>' +
-            '<tr>'
-            );
+//function getMenuItems() {
+//    $.get("/api/menuItem/", function (data) {
+//        if (data.length <= 0) {return;}
+//        $('#menuItem').empty();
+//        for (var i = 0; i < data.length; i++) {
+//            $('#menuItem').append(
+//            '<tr>' +
+//                '<td>' + data[i].menuItemName + '</td>' +
+//                '<td>' + data[i].menuItemPrice + '</td>' +
+//            '<tr>'
+//            );
+//        }
+//    });
+//}
+//
+//function saveMenuItems(b) {
+//    b.preventDefault();
+//
+//    var nameMI = $("#nameMenuItem").val();
+//    var priceMI = $("#priceMenuItem").val();
+//
+//    $.post("/api/menuItem/add",{
+//        menuItemName: nameMI,
+//        menuItemPrice: priceMI
+//      },
+//    getMenuItems
+//    )
+//};
+//
+////function removeIngredient(){
+////    $.get("/api/ingredient/remove/{id}", function(data){
+////        if(data.length<=0){return;}
+////        var html = "";
+////        for(var i=0;i<data.length;i++){
+////        html +="<p>" + data[i].ingredientId;
+////        data[i].ingredientId = id;
+////    },)};
+//
+//
+//$("#saveMenuItem").submit(saveMenuItems);
+
+
+function postData() {
+    console.log("posting data...");
+
+    // Get values from html.
+    var name = $("#nameMenuItem").val();
+    var price = $("#priceMenuItem").val();
+
+
+    // Create JS object with data.
+    var newFood = {
+        menuItemName : name,
+        menuItemPrice : price
+    };
+    console.log(newFood);
+
+    // Convert JS object to JSON.
+    var validJsonFood = JSON.stringify(newFood);
+    console.log(validJsonFood);
+
+    // Post JSON to endpoint.
+    $.ajax({
+        url:"/api/menuItem/add",
+        type:"post",
+        data: validJsonFood,
+        contentType: "application/json",
+        success: function(result) {
+            // On successful post, reload data to get the added one as well.
+            console.log("success post data!");
+            getData();
         }
     });
 }
 
-function saveMenuItems(b) {
-    b.preventDefault();
+function getData() {
+    console.log("getting data...");
 
-    var nameMI = $("#nameMenuItem").val();
-    var priceMI = $("#priceMenuItem").val();
+    // Get the data from endpoint.
+    $.ajax({
+        url:"/api/menuItem/",
+        type:"get",
+        success: function(foods) {
+            // On successful get, reload the datatable with new data.
+            console.log("This is the data: " + foods);
+            $('#ingredients').DataTable().clear();
+            $('#ingredients').DataTable().rows.add(ingredients);
+            $('#ingredients').DataTable().columns.adjust().draw();
+        }
+    });
+}
 
-    $.post("/api/menuItem/add",{
-        menuItemName: nameMI,
-        menuItemPrice: priceMI
-      },
-    getMenuItems
-    )
-};
+function setup() {
+    // Modal submit.
+    $("#saveFood").on('submit', function(e) {
+        console.log("Submitted new menu item form");
 
-//function removeIngredient(){
-//    $.get("/api/ingredient/remove/{id}", function(data){
-//        if(data.length<=0){return;}
-//        var html = "";
-//        for(var i=0;i<data.length;i++){
-//        html +="<p>" + data[i].ingredientId;
-//        data[i].ingredientId = id;
-//    },)};
+        // Post the data from the modal.
+        postData();
 
+        // Reset modal to hide and no values.
+        $('#newFoodModal').modal('hide');
+        $("#nameMenuItem").val("");
+        $("#priceMenuItem").val("");
+    });
 
-$("#saveMenuItem").submit(saveMenuItems);
+    // Load DataTable with data format.
+    $('#foods').DataTable({
+        columns: [
+            { "data": "menuItemName" },
+            { "data": "menuItemPrice" }
+        ]
+    });
+
+    // Load first data.
+    getData();
+}
