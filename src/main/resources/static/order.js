d2ccdddb53955054e57cad9a14207bebb8c32ad8
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-05-07 13:47:07 +0200

    READY ORDER!

--- src/main/resources/static/order.js
+++ src/main/resources/static/order.js
@@ -15,9 +15,6 @@
         $('#table').append(
             '<tr>' +
             '<td class="h5">' + dataFood[i].menuItemName + '</td>' +
-            '<td>' +
-            '</td>' +
-            '<td><button class="btn btn-grey btn-sm mr-1">-</button>0<button class="btn btn-grey btn-sm ml-1">+</button></td>' +
             '<td>' + dataFood[i].menuItemPrice + '</td>' +
             '</tr>'
         );
