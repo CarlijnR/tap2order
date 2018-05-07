8ff058fa2a2fe4674ae728a16a4dbbdf647c6a36
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-05-07 10:55:11 +0200

    Typos

--- src/main/resources/static/food.js
+++ src/main/resources/static/food.js
@@ -43,17 +43,17 @@ function getDataFood() {
         getDataFood();
     });
 
-$.get("api/restaurant/menu/list/food", function(data){
-    for(var i=0; i < data.length;i++){
-        $('#foodlist').append(
-            '<tr>' +
-            '<td class="h5">' + data[i].name + '</td>' +
-            '<td>' + "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim." +
-            '</td>' +
-            '<td><button class="btn btn-grey btn-sm mr-1">-</button>0<button class="btn btn-grey btn-sm ml-1">+</button></td>' +
-            '<td>' + data[i].price + '</td>' +
-            '</tr>'
-        );
-    }
-})
+//$.get("api/restaurant/menu/list/food", function(data){
+//    for(var i=0; i < data.length;i++){
+//        $('#foodlist').append(
+//            '<tr>' +
+//            '<td class="h5">' + data[i].name + '</td>' +
+//            '<td>' +
+//            '</td>' +
+//            '<td><button class="btn btn-grey btn-sm mr-1">-</button>0<button class="btn btn-grey btn-sm ml-1">+</button></td>' +
+//            '<td>' + data[i].price + '</td>' +
+//            '</tr>'
+//        );
+//    }
+//})
 
