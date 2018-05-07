900d0fddeca69441f83c5dc4844917d801415e4d
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-05-07 10:42:37 +0200

    Selecting Menu (working)

--- src/main/resources/static/food.js
+++ src/main/resources/static/food.js
@@ -1,78 +1,3 @@
-//function getMenuItems(){
-//    $.get("/api/menuItem/", function(data){
-//    if(data.length<=0){return;}
-//    var html = "";
-//    for(var i=0;i<data.length;i++){
-//    html +="<p>" + data[i].menuItemName + " - " + data[i].menuItemPrice}
-//    $("#menuItem").html(html);
-//    });
-//}
-//
-//function getDrinkItems(){
-//    $.get("/api/drink/", function(data){
-//    if(data.length<=0){return;}
-//    var html = "";
-//    for(var i=0;i<data.length;i++){
-//    html +="<p>" + data[i].drinkName + " - " + data[i].drinkPrice}
-//    $("#drinksText").html(html);
-//    });
-//}
-
-
-
-//function printOrder(e)
-//{
-//e.preventDefault();
-//        $.get("/api/restaurant/order/printOrder", function(data){
-//        console.log("Enters this shit");
-//        if (data.length<=0)
-//        {
-//           console.log("empty output");
-//            return;
-//        }
-//        var string="";
-//        for(var i=0; i<data.length;i++){
-//            string += "<p>" + data[i] + "</p>";
-//            console.log(data[i]);
-////            for(var j=0; j<data[i].menuItemList.name;j++){
-////            console.log(data[i].menuItemList.name);
-////            console.log("New Item");
-////        }
-//        console.log("After");
-//
-//
-//}
-//});
-//}
-//
-//function menuItem(b)
-//{
-//b.preventDefault();
-//$.get("api/restaurant/menu/list", function(data){
-//    console.log("Checking this list");
-//            if (data.length<=0)
-//            {
-//               console.log("empty list");
-//               return;
-//            }
-//            var string="";
-//            for(var i=0; i<data.length;i++){
-//            console.log(data[i]);
-//                string += "<p>" + data[i].name + "- $" + data[i].price;
-//
-//    //            for(var j=0; j<data[i].menuItemList.name;j++){
-//    //            console.log(data[i].menuItemList.name);
-//    //            console.log("New Item");
-//    //        }
-//            console.log("After");
-//            }
-//            $("#menu-page-show").html(string);
-//});
-//}
-//
-//$("#foodbutton").click(menuItem);
-
-
 function getDataFood() {
 
 
@@ -94,13 +19,28 @@ function getDataFood() {
 
     $(document).ready(function () {
         console.log("tadaafood");
-          $('#menuItem').DataTable({
+          var events = $("#events");
+          var table = $('#menuItem').DataTable({
                 columns: [
                     { "data": "menuItemName" },
                     { "data": "menuItemPrice" }
-                ]
+                ],
+                select:{ style: 'multi'},
             });
 
+            table
+                .on ('select.dt', function(e, type){
+                    var rowData = table.rows('.selected').data().toArray();
+                                console.log("enters rowData function");
+                                events.prepend('<div><b>'+type+' selection</b> - '+JSON.stringify(rowData)+'</div>' );
+                                console.log(rowData);
+                            } )
+                .on( 'deselect', function ( e, dt, type, indexes) {
+                     console.log("enters .on function");
+                     var rowData = table.rows(indexes).data().toArray();
+                     events.prepend( '<div><b>'+type+' <i>de</i>selection</b> - '+JSON.stringify( rowData )+'</div>' );
+                     console.log(rowData);
+                    });
         getDataFood();
 
     });
